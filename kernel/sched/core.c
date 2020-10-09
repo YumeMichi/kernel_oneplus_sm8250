a12353b52213dadc8c39c3045c2dad13c9bf9744
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2020-10-09 21:52:57 +0200

    rtmutex: Clean up the RT patches
    This is an all-in-one commit of the clean up of the RT-MUTEX related
    patches. A few patches were dropped, other where replaced, restructed
    and extended. This includes:
    
    - Remove unused code, struct members.
    
    - Add a scheduling function to the scheduler which is invoked
      instead of schedule() while blocking on a sleeping lock. This
      allows to removes
      - the extra flush-block-io calls.
      - the sleeping lock counter.
    
      As a result blocking on a sleeping lock from within a
      (workerqueue/IO) worker thread won't fire up another worker. On
      !PREEMPT_RT this does not happen if the execution blocks on a
      spinlock_t. This change aligns the behaviour. Further testing is
      required to see if this behaviour is desired for PREEMPT_RT.
    
    - Disable migration after the sleeping lock has been acquired. This
      has been done before the lock was acquired due to CPU hotplug
      restriction. These restrictions have been lifted and there should
      be nothing requiring the old behaviour. The advantage is that the
      scheduler is able to freely place the task once the lock is
      available. Otherwise the task would have to wait until the CPU is
      no longer by a task with a higher priority. This of course doesn't
      work for tested locks.
    
    Change-Id: I85e9ecff3a1edae6ebc5d1953ec44dfd839e6c9f
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    [Sultan Alsawaf: backport to 4.14]
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4360,7 +4360,7 @@ static void migrate_disabled_sched(struct task_struct *p);
  *
  * WARNING: must be called with preemption disabled!
  */
-static void __sched notrace __schedule(bool preempt)
+static void __sched notrace __schedule(bool preempt, bool spinning_lock)
 {
 	struct task_struct *prev, *next;
 	unsigned long *switch_count;
@@ -4400,7 +4400,7 @@ static void __sched notrace __schedule(bool preempt)
 	update_rq_clock(rq);
 
 	switch_count = &prev->nivcsw;
-	if (!preempt && prev->state) {
+	if ((!preempt || spinning_lock) && prev_state) {
 		if (unlikely(signal_pending_state(prev->state, prev))) {
 			prev->state = TASK_RUNNING;
 		} else {
@@ -4467,7 +4467,7 @@ void __noreturn do_task_dead(void)
 	/* Tell freezer to ignore us: */
 	current->flags |= PF_NOFREEZE;
 
-	__schedule(false);
+	__schedule(false, false);
 	BUG();
 
 	/* Avoid "noreturn function does return" - but don't continue if BUG() is a NOP: */
@@ -4493,9 +4493,6 @@ static inline void sched_submit_work(struct task_struct *tsk)
 		preempt_enable_no_resched();
 	}
 
-	if (tsk_is_pi_blocked(tsk))
-		return;
-
 	/*
 	 * If we are going to sleep and we have plugged IO queued,
 	 * make sure to submit it to avoid deadlocks.
@@ -4517,7 +4514,7 @@ asmlinkage __visible void __sched schedule(void)
 	sched_submit_work(tsk);
 	do {
 		preempt_disable();
-		__schedule(false);
+		__schedule(false, false);
 		sched_preempt_enable_no_resched();
 	} while (need_resched());
 	sched_update_worker(tsk);
@@ -4545,7 +4542,7 @@ void __sched schedule_idle(void)
 	 */
 	WARN_ON_ONCE(current->state);
 	do {
-		__schedule(false);
+		__schedule(false, false);
 	} while (need_resched());
 }
 
@@ -4598,7 +4595,7 @@ static void __sched notrace preempt_schedule_common(void)
 		 */
 		preempt_disable_notrace();
 		preempt_latency_start(1);
-		__schedule(true);
+		__schedule(true, false);
 		preempt_latency_stop(1);
 		preempt_enable_no_resched_notrace();
 
@@ -4654,6 +4651,19 @@ asmlinkage __visible void __sched notrace preempt_schedule(void)
 NOKPROBE_SYMBOL(preempt_schedule);
 EXPORT_SYMBOL(preempt_schedule);
 
+#ifdef CONFIG_PREEMPT_RT_FULL
+void __sched notrace preempt_schedule_lock(void)
+{
+	do {
+		preempt_disable();
+		__schedule(true, true);
+		sched_preempt_enable_no_resched();
+	} while (need_resched());
+}
+NOKPROBE_SYMBOL(preempt_schedule_lock);
+EXPORT_SYMBOL(preempt_schedule_lock);
+#endif
+
 /**
  * preempt_schedule_notrace - preempt_schedule called by tracing
  *
@@ -4700,7 +4710,7 @@ asmlinkage __visible void __sched notrace preempt_schedule_notrace(void)
 		 * an infinite recursion.
 		 */
 		prev_ctx = exception_enter();
-		__schedule(true);
+		__schedule(true, false);
 		exception_exit(prev_ctx);
 
 		preempt_latency_stop(1);
@@ -4729,7 +4739,7 @@ asmlinkage __visible void __sched preempt_schedule_irq(void)
 	do {
 		preempt_disable();
 		local_irq_enable();
-		__schedule(true);
+		__schedule(true, false);
 		local_irq_disable();
 		sched_preempt_enable_no_resched();
 	} while (need_resched());
