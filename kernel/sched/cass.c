4490b54ee5cf5e066e6c2744324eaeba65954a07
Yuan Si <do4suki@gmail.com>
2024-01-28 19:11:17 +0800

    Revert "sched/cass: Update sched_util_threshold for prime core"
    This reverts commit cfd2f854cb560ef03836a8614d7e917adb6b94e8.
    
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -33,7 +33,7 @@ struct cass_cpu_cand {
 };
 
 unsigned int sched_util_threshold[NR_CPUS] = {
-	646, 646, 646, 646, 500, 500, 500, 450
+	646, 646, 646, 646, 500, 500, 500, 404
 };
 
 static __always_inline
